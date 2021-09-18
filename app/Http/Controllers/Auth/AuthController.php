58b22cbaf938c6e1a3d76f3945152adcab86cef4
yoshinori0811 <grapemix000@gmail.com>
2021-09-19 00:37:44 +0900

    create login/logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
 
     /**
+     * ログインフォームの表示
      * @return View
      */
     public function showLogin()
@@ -18,14 +20,53 @@ class AuthController extends Controller
     }
 
     /**
+     * ログイン処理
      * @param App\Http\Requests\LoginFormRequest $request
      */
     public function login(LoginFormRequest $request)
     {
-        dd($request->all());
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        }
+
+        return back()->withErrors([
+            'danger' => 'メールアドレスかパスワードが間違っています。',
+        ]);
+
+    }
+    
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('showLogin')->withErrors([
+            'danger' => 'ログアウトしました。',]);
     }
 
 
+
+    /**
+     * ホーム画面表示
+     * 
+     */
+    public function showHome()
+    {
+        return view('login.home');
+    }
 
 
 }
